ab283a75137cc6d7f06d558f34bdb6e605038e85
Rudraksh <khandelwalr207@gmail.com>
2025-06-13 18:25:42 +0530

    refactored inc and dec buttons

--- src/features/cart/UpdateItemQuan.jsx
+++ src/features/cart/UpdateItemQuan.jsx
@@ -6,18 +6,18 @@ export default function UpdateItemQuantitites({ pizzaId, currentQuantity }) {
   const dispatch = useDispatch();
   return (
     <div className="flex items-center gap-1 md:gap-3">
-      <Button
-        type="round"
-        onClick={() => dispatch(increaseItemQuantity(pizzaId))}
-      >
-        +
-      </Button>
       <span className="text-sm font-medium">{currentQuantity}</span>
       <Button
         type="round"
         onClick={() => dispatch(decreaseItemQuantity(pizzaId))}
       >
         -
+      </Button>
+      <Button
+        type="round"
+        onClick={() => dispatch(increaseItemQuantity(pizzaId))}
+      >
+        +
       </Button>
     </div>
   );
